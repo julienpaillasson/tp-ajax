4f590e438d11f94d795c38c665f7f26d1b4c0db9
Sinostra <spanjaard.david@gmail.com>
2017-06-21 10:35:57 +0200

    Ajout du fichier api.php

---
+++ basic-87/php/api.php
@@ -0,0 +1,39 @@
+<?php 
+
+try {
+	$pdo = new PDO("mysql:host=localhost;dbname=mikemonroi","root","");
+	$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+}
+
+catch (PDOException $e) {
+	echo "Message d'erreur : ".$e;
+}
+
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+	$request = "INSERT INTO vehicules (Marque, Modele, Annee, Couleur, Image) VALUES (:Marque, :Modele, :Annee, :Couleur, :Image)";
+
+	$prep = $pdo->prepare($request);
+
+	$prep->bindValue(':Marque', $_POST['Marque'], PDO::PARAM_STR);
+	$prep->bindValue(':Modele', $_POST['Modele'], PDO::PARAM_STR);
+	$prep->bindValue(':Annee', $_POST['Annee'], PDO::PARAM_STR);
+	$prep->bindValue(':Couleur', $_POST['Couleur'], PDO::PARAM_STR);
+	$prep->bindValue(':Image', $_POST['Image'], PDO::PARAM_STR);
+	$prep->execute();
+
+}
+
+else if($_SERVER['REQUEST_METHOD'] == 'GET') {
+
+	$request = "SELECT * FROM vehicules";
+
+	$prep = $pdo->prepare($request);
+	$result = $prep->execute();
+
+	echo json_encode($result->fetchAll(PDO::FETCH_ASSOC));
+
+}
+
+
+?>
